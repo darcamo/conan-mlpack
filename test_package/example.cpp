c9f2a49516ce6b8223fd1f6b9b944b4e8c5c9ef2
Darlan Cavalcante Moreira <darcamo@gmail.com>
2018-09-05 22:33:54 -0300

    Fix example code

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,13 +1,23 @@
 #include <mlpack/methods/linear_regression/linear_regression.hpp>
+
 using namespace mlpack::regression;
 
 int main() {
-    arma::mat data; // The dataset itself.
-    arma::vec responses; // The responses, one row for each row in data.
 
-    data.randn(2,2);
-    responses.eye(2,2);
+    // Generate some data and response
+    arma::mat data = arma::randn<arma::mat>(20, 2);
+    arma::rowvec responses(20);
 
-    LinearRegression lr(data, responses);
+    // For this test the response will be perfect linear
+    for(unsigned int i = 0; i < 20; i++) {
+        responses(i) = arma::as_scalar(data.row(i) * arma::vec{1, -4});
+    }
+
+    // Note that in mlpack the features are the rows, while the columns are the
+    // samples -> That is why we have a transpose here
+    LinearRegression lr(data.st(), responses);
     arma::vec parameters = lr.Parameters();
+
+    parameters.print("Regression Parameters: ");
+    arma::vec{0, 1, -4}.print("Correct parameters:");
 }
